9ff0517d150e867c85c141ea73c7ec787f28b570
Mikko Ohtamaa <mikko@opensourcehacker.com>
2016-09-09 02:58:34 +0300

    Back to development: 0.1.4

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ extras_require = {
 
 setup(
     name='pyramid_sms',
-    version='0.1.3',
+    version='0.1.4.dev0',
     description="SMS service framework for Pyramid",
     long_description=readme + '\n\n' + history,
     author="Mikko Ohtamaa",
